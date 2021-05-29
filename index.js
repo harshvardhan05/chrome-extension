4a993223b7678ac97926dab46a715fc8ff6ce590
Harshvardhan <hv050694@gmail.com>
2021-05-29 18:47:57 +0530

    second commit

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const inputBtn = document.querySelector("#input-btn")
 const inputEl = document.querySelector("#input-el")
 const ulEl = document.querySelector("#ul-el")
-const liEl = document.querySelector("#li-el")
+// const liEl = document.querySelector("#li-el")
+
+
 
 let myLeads = []
-
-
-inputBtn.addEventListener("click",()=>{
+inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    ulEl.textContent = " "
-    // console.log(myLeads)
-    for(let i=0; i<myLeads.length; i++){
-        ulEl.textContent += myLeads[i] 
-    }
+    console.log(myLeads)
 })
 
-console.log(myLeads)
+let listItems = ""
+
+for(let i=0; i<myLeads.length; i++){
+    listItems += "<li>" +  myLeads[i]  + "</li>"
+    
+   /* another way of doing it is :
+            create element
+            set text content
+            append to ul
+    
+   const liEl = document.createElement("li")
+   liEl.textContent = myLeads[i]
+   listItems.append(liEl)
+   */
+}
+ulEl.innerHTML = listItems
