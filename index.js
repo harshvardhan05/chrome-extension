d8adce3582f4d434fec868da6d9d2b1e74ad400b
Harshvardhan <hv050694@gmail.com>
2021-06-02 02:22:49 +0530

    final commit

--- index.js
+++ index.js
@@ -2,31 +2,54 @@ let myLeads = []
 const inputBtn = document.querySelector("#input-btn")
 const inputEl = document.querySelector("#input-el")
 const ulEl = document.querySelector("#ul-el")
-// const liEl = document.querySelector("#li-el")
+const delBtn = document.querySelector("#delete-btn")
+const tabBtn = document.querySelector("#tab-btn")
+//<----------------get the leads from localstorage---------------->
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 
-
-// let myLeads = [] 
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    // console.log(myLeads)
-    inputEl.value = null
-    renderLeads()
+//<--------------get leads from the current tab --------->
+tabBtn.addEventListener("click", function(){
+// myLeads.push(window.location.href)
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })    
 })
 
-function renderLeads(){
+//<-----checking leads from local storage ---------->
+if(leadsFromLocalStorage){
+  myLeads = leadsFromLocalStorage
+  render(myLeads)
+}
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = null
+//<----Save The Leads To Local Storage--->
+    // myLeads = JSON.stringify(myLeads)
+    // localStorage.setItem("myLeads", myLeads)
+    // myLeads = JSON.parse(myLeads)          
+    //      <------OR------->
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+//<-------------rendering leads --------->
+function render(leads){
     let listItems = ""
-    for(let i = 0; i < myLeads.length; i++){
-    //   listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" +  myLeads[i]  + "</a></li>"
+    for(let i = 0; i < leads.length; i++){
+//   listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" +  myLeads[i]  + "</a></li>"
+//<-----------Template String -------------->
          listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'> 
-                    ${myLeads[i]} 
+                <a target='_blank' href='${leads[i]}'> 
+                    ${leads[i]} 
                 </a>
             </li>
-        `
-        
-       // console.log(listItems)
+        ` 
+    // console.log(listItems)
             //     another way of doing it is :
             //     create element
             //     set text content
@@ -37,3 +60,10 @@ function renderLeads(){
     }
     ulEl.innerHTML =  listItems 
 } 
+
+delBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+// ulEl.textContent = null
+})
